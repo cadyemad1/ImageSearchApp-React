3a28ee56ee75f2921c511e4fa5936b1e5cd2ed3e
Cady Emad <cadyemad@Cadys-MacBook-Air.local>
2019-05-08 15:45:35 +0200

    Updates

---
+++ precache-manifest.0b163aa7f32076e765817a755bd5df08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcd121bb38735c9f99f2",
+    "url": "/ImageSearchApp-React/static/js/runtime~main.a1fda484.js"
+  },
+  {
+    "revision": "a8becf21ff6b22597bfe",
+    "url": "/ImageSearchApp-React/static/js/main.37005a2b.chunk.js"
+  },
+  {
+    "revision": "fad40f60982e060bf6fa",
+    "url": "/ImageSearchApp-React/static/js/2.2e4d52be.chunk.js"
+  },
+  {
+    "revision": "a8becf21ff6b22597bfe",
+    "url": "/ImageSearchApp-React/static/css/main.dbcf6e39.chunk.css"
+  },
+  {
+    "revision": "4f55c13f00c5c3acb80a94a03984c0f2",
+    "url": "/ImageSearchApp-React/index.html"
+  }
+];
